ca0eae05ba49392d3df358760ddc51a9eaf9046c
Unknown <andra.vasilcoiu@yahoo.com>
2019-05-07 10:09:10 +0100

    Changed part2

--- assignment2_12345.pl
+++ assignment2_12345.pl
@@ -32,9 +32,9 @@ solve_task(Task,Cost) :-
 % stupid method not quite working.
 try_part4(_, [_, []]) :- true.
 
-try_part4(Task, [Agent, Path]) :- 
+try_part4(Task, [Agent, Path]) :-
   Path = [H|T],
-  (query_world(check_pos, [H, empty]) -> 
+  (query_world(check_pos, [H, empty]) ->
     query_world(agent_do_moves, [Agent, [H]]),
     try_part4(Task, [Agent, T]);
     otherwise -> write("I'm Blocked, Need A New Path!!"),nl,
