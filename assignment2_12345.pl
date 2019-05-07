48a29be9805bc38958cde9e8964a43b8ec94bc7e
Abby <wzr314@me.com>
2019-05-08 00:11:54 +0100

    Oh God

--- assignment2_12345.pl
+++ assignment2_12345.pl
@@ -22,58 +22,68 @@ solve_general(Task,Cost) :-
 
 
 solve_task(Task,Cost) :-
-  b_setval(target_pos,Task),
-  (Task = go(_) -> nb_setval(opt, 1);
-   otherwise -> nb_setval(opt, 0)),
-  my_agent(Agent),
-  energy_status(Agent),
-  query_world( agent_current_position, [Agent,P] ),
+  b_setval(destination,Task),
+  ( Task = go(_) -> nb_setval(flag, 1);
+   otherwise -> nb_setval(flag, 0)
+  ),
+  my_agent(A),
+  energy_status(A), % cheking energy
+  query_world( agent_current_position, [A,P] ),
   solve_task_Astar(Task, [[c(0, 0, P), P]], R, Cost, _NewPos, []),!,
-  nb_getval(opt, Opt),
-  (Opt = 1 -> reverse(R,[_Init|Path]),query_world( agent_do_moves, [Agent,Path] );
-  otherwise -> R = [Last|_],solve_task(go(Last),_)).
-
-
-robustness(_,[_,[]]).
-robustness(Task,[Agent,[Head|Tail]]) :-
-  (query_world( check_pos,[Head,empty] ) ->
-  query_world(agent_do_moves,[Agent, [Head]]),robustness( Task,[Agent,Tail] );
-  otherwise -> write("I can't advance, need another path!!"),
-  nl,
-  solve_task_part4_o( Task,_ )
+  nb_getval(flag, Flag),
+  ( Flag = 1 -> reverse( R,[_Init|Path]),
+    query_world( agent_do_moves, [A,Path] );
+    otherwise -> R = [Last|_], solve_task( go(Last),_)
   ).
 
-solve_task_part4_o(goto_another_oracle( o(X) ),Cost) :-
-  my_agent( Agent ),
-  energy_status( Agent ),
-  query_world(agent_current_position,[Agent, P]),
-  solve_bfs( goto_another_oracle( o(X) ), [[c( 0, 0, P ), P]], R, Cost, _NewPos, []),
+
+solve_task_part4_o( goto_another_oracle( o(X) ), Cost) :-
+  my_agent( A ),
+  % always check energy before a move
+  energy_status( A ),
+  query_world( agent_current_position, [A, P]),
+  solve_bfs( goto_another_oracle( o(X) ),
+  [[c( 0, 0, P ), P]], R, Cost, _NewPos, []),
   !,
   reverse(R, [_Init | Path] ),
-  robustness( goto_another_oracle( o(X) ),[Agent,Path] ) ;
-  query_world(agent_do_moves,[Agent, Path]).
+  robustness( goto_another_oracle( o(X) ), [A, Path] ) ;
+  query_world( agent_do_moves, [A, Path]).
+
+
+% robustness search for part4
+robustness( Task, [Agent, [] ] ).
+robustness( Task, [A, [H|T] ] ) :-
+  (query_world( check_pos,[H,empty] ) ->
+  query_world(agent_do_moves, [A, [H] ]),
+  robustness( Task, [A, T] );
+  otherwise -> write("World changing fast, I'm finding another path!!"),
+  nl,
+  solve_task_part4_o( Task, _ )
+  ).
 
 
 %%%%%%%%%%%%%%%%%%
-%% for part3
+%% for part3 %%
 
 % checking, set the lowest energy here
-energy_status( Agent ) :-
-  query_world( agent_current_energy,[Agent,E] ),
-  (E>50->true; otherwise->solve_task_o(find(c(_)),_)).
+energy_status( A ) :-
+  query_world( agent_current_energy,[A, E] ),
+  % set energy threshold here
+  (E>50->true; otherwise->solve_task_o(find(c(_)),_)). 
 
 
 % in case you run out of energy
 solve_task_o( find( c(X) ), Cost ) :-
   my_agent( Agent ),
   query_world(agent_current_position,[Agent, P]),
-  solve_bfs( find(c(X)), [[c(0, 0, P), P]], R, Cost, _NewPos, []),
+  solve_bfs( find(c(X)), [[c(0, 0, P), P]],
+  R, Cost, _NewPos, []),
   !,
   reverse( R, [_Init | Path] ),
   query_world( agent_do_moves, [Agent,Path] ),
   query_world(agent_topup_energy, [Agent,c(X)] ),
   query_world( agent_current_energy, [Agent,E] ),
-  write("Current energy is "),
+  write("Finished topop, current energy is "),
   write(E),
   nl.
 
@@ -94,7 +104,7 @@ solve_task_o( goto_another_oracle( o(X) ), Cost ) :-
 solve_bfs( Task,[Current | _],R,CostsBFS,NewPos,_) :-
   Current = [c(_, G, P) | RPath],
   CostsBFS = [cost( Cost ), depth( G )],
-  achieved( Task,[c(G, P) | RPath],R,Cost,NewPos ).
+  achieved( Task, [c(G, P) | RPath], R, Cost, NewPos ).
 
 
 solve_bfs(Task,[Current|Rest],RR,Cost,NewPos,Explored):-
@@ -136,19 +146,20 @@ solve_task_bt(Task,Current,D,RR,Cost,NewPos) :-
 
 
 %%%% Agenda based A star %%%%%%%%%%%%%%%%%%%%%
-solve_task_Astar(Task,[Current|_],RPath,Cost,NewPos,_):-
+solve_task_Astar(Task, [Current|_], RPath, Cost, NewPos, _):-
   achieved_Astar(Task,Current,RPath,Cost,NewPos).
 
-solve_task_Astar(Task,[Current|Agenda],RR,Cost,NewPos,Closelist) :-
-  Current = [c(_,G,P)|RPath],
-  G1 is G + 1,
+solve_task_Astar(Task,[Current|Agenda],RR,Cost,NewPos,Closest) :-
+  Current = [c(_, G,P)|RPath], G1 is G + 1,
   % add children to the agenda
-  (setof([c(F1,G1,Pos1),Pos1|RPath], search_Astar(P,Pos1,F1,G1,Closelist), Children)
+  (setof([c(F1,G1,Pos1),Pos1|RPath],
+   search_Astar(P,Pos1,F1,G1,Closest), Children)
   -> merge(Agenda, Children, NewAgenda);
-  NewAgenda = Agenda),
+  NewAgenda = Agenda
+  ),
+  exclude(memberchk(Closest), NewAgenda, FilteredAgenda),
+  solve_task_Astar(Task,FilteredAgenda,RR,Cost,NewPos,[P|Closest]).
 
-  exclude(memberchk(Closelist), NewAgenda, FilteredAgenda),
-  solve_task_Astar(Task,FilteredAgenda,RR,Cost,NewPos,[P|Closelist]).
 
 achieved_Astar(go(Exit),Current,RPath,Cost,NewPos) :-
   Current = [c(_,Cost,NewPos)|RPath],
@@ -159,25 +170,24 @@ achieved_Astar(go(Exit),Current,RPath,Cost,NewPos) :-
 achieved_Astar(find(O),Current,RPath,Cost,NewPos) :-
   Current = [c(_,Cost,NewPos)|RPath],
   ( O=none    -> true
-  ; otherwise -> memberchk(Last, RPath),map_adjacent(Last,_,O)
+  ; otherwise -> memberchk(Last, RPath),
+  map_adjacent( Last, _, O)
   ),
   otherwise -> true.
 
 
-cal_manhattan_dis(Pos, Goal, Dist):-
-  Pos = p(X_0,Y_0),
-  Goal = p(X_1,Y_1),
-  Dist is abs(X_0-X_1) + abs(Y_0-Y_1).
+manhattan_distance(Pos, Goal, Distance):-
+  Pos = p(X1,Y1), Goal = p(X2,Y2),
+  Distance is abs(X1-X2) + abs(Y1-Y2).
 
-search_Astar(P,N,F1,G1,Closelist):-
-  map_adjacent(P,N,empty),
-  \+ memberchk(N,Closelist),
-  nb_getval(opt, Opt),
 
-  (Opt = 1 -> b_getval(target_pos, go(Goal)),
-  cal_manhattan_dis(N, Goal, Dist),
-  H is Dist;
-  otherwise -> H is 0),
+search_Astar(P,N,F1,G1,Closest):-
+  map_adjacent(P,N,empty), \+ memberchk(N,Closest),
+  nb_getval(flag, Flag),
+  (Flag = 1 -> b_getval( destination, go(Goal)),
+  manhattan_distance(N, Goal, Distance), H is Distance;
+  otherwise -> H is 0
+  ),
   F1 is H + G1.
 
 
@@ -189,6 +199,7 @@ achieved(go(Exit),Current,RPath,Cost,NewPos) :-
   ; otherwise -> RPath = [Exit|_]
   ).
 
+
 achieved(find(O),Current,RPath,Cost,NewPos) :-
   Current = [c(Cost,NewPos)|RPath],
   ( O=none    -> true
@@ -196,7 +207,7 @@ achieved(find(O),Current,RPath,Cost,NewPos) :-
   ).
 
 
-%%% for part3
+%%%  for part3  %%%
 
 achieved(goto_another_oracle(O),Current,RPath,Cost,NewPos) :-
   my_agent(Agent),
